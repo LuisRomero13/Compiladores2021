8c12b1ce6531932e2385dea56eaba0f1067b0056
Luis Romero <luis-romero96@hotmail.com>
2021-09-04 04:39:04 -0300

    automata posible final,inicio excel y an.lex

--- an.lex.c
+++ an.lex.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
+#include<string.h>
+#include<ctype.h>
+
+struct token {
+      int numeroToken;
+      char id[25];
+};
 
 int main(){
+    struct token tokenFinal;
+
     printf("holi");
     getch();
     system("pause");
